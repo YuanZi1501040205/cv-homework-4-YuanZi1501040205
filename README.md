9d91f922fae3a7688f7df0258178f680fec1740f
shah4397 <sshah@central.uh.edu>
2020-04-19 17:26:12 -0500

    Setup HW4

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# CV_Homework_4
+# CV Homework #4
+
+CV Homework 4: Object Detection and Tracking
+
+Due Date: 11:59 PM, 6th May 2020
+
+See Homework-4 description file (Homework-4.pdf) for details.
+
+Object detection and tracking is a common requirement for a range of applications in computer vision.  In this assignment, we will leverage the broad understanding we have developed throughout this semester, specifically as it related to background modeling for foreground object detection, Kalman filtering for prediction of object location, and jointly, for object tracking.  
+
+This homework assignment will focus on the design of an object tracking solution that is implemented under a 2 module framework; a module for detection of foreground objects, and a module for tracking individual detected objects while ensuring consistent object id association using distance as a metric. 
+
+You are given a solution template that includes the main function that implements the overall object tracking solution as shown above.  In addition, the Track module is also available that already includes implementation of the Kalman filter that uses the centroid of detected objects as its state parameter.  The focus of this assignment will be on implementing the Detect module.  In doing so, you will also be required to implement appropriate Background Modeling approach to perform Background Subtraction and Foreground Object Detection.  
+
+Specific assignment steps are:
+
+1. Background modeling (5 Pts.) - In the given BGModel() class (background_model.py), you are to implement the following background subtraction methods:
+  
+    a. Background model estimated by computing the mean value of observed image intensity for each pixel
+    
+    b. Background model estimated by computing the median of observed image intensity for each pixel
+    
+    c. Background model estimated by modeling each pixel’s intensity as a Gaussian (mean and variance) 
+    
+    d. Background model estimated by modeling each pixel’s intensity as a Mixture of Gaussians (can use cv2.createBackgroundSubtractorMOG2())
+    
+    e. Background model estimated by modeling each pixel’s intensity based on Nearest Neighbor estimate of observed intensities (can use cv2. createBackgroundSubtractorKNN())
+
+**Note that the specific computation of the foreground image after background subtraction should be performed in the compute_fgmask() method
+**
+
+2.	Object detection (20 Pts.) - In the given Detectors() class (detectors.py), you are to implement an approach to detect objects for tracking and their centroid values.
+  - The detector should allow for use of one of the background subtraction methods and get the foreground image.  This should be further processed to detect objects worthy of tracking.
+  - Your choice of processing to detect objects and compute their centroids should be implemented in the Detect() method within the class. (Please note that this does not require classifier based detection of objects)
+
+3.  You should not modify the following files:
+4.            cv_hw4.py, kalman_filter.py and tracker.py files.
+
+4.	Report (5 Pts.) - Write a report outlining your approach for background subtraction and object detection.
+  - Discuss which background model is better, and why. 
+  - Discuss how different background model effects your detection. 
+  - Describe the disadvantage of your detection, and discuss the potential improvement you can think of.
+  
+Your solution needs to ensure that:
+
+1. The homework has to run using commands:
+
+           python cv_hw4.py -v QIL_orig.mp4 -b mean -d 0 
+           python cv_hw4.py -v QIL_orig.mp4 -b median -d 0  
+           python cv_hw4.py -v QIL_orig.mp4 -b gaussian -d 0  
+           python cv_hw4.py -v QIL_orig.mp4 -b mog -d 0  
+           python cv_hw4.py -v QIL_orig.mp4 -b knn -d 0  
+
+2. Result image will be written to the output output/ folder.
+
+
+General Instructions:
+
+1. Submit code on Github
+1. All coding must be done using python
+1. Use tools available in OpenCV.
+
+Submission Instructions:
+
+1. Submit only the source code files.
+1. The TA will have the OpenCV setup in his development environment and will paste your code and run it. Do not use any third-party libraries as the TA will not have access to it. 
+1. Include a readme file to describe any required set up, like location of image files that was hard coded, or any other information that is required to run the code
+
+Grading:
+
+1. Background modelling   - 5 Pts. 
+1. Object detection       - 20 Pts.
+1. Report                 - 5 Pts. 
+
+Total                 - 30 Pts.
---
+++ cv_hw4.py
@@ -0,0 +1,171 @@
+# Import python libraries
+import numpy as np
+import cv2
+import copy
+from qil.detectors import Detectors
+from qil.tracker import Tracker
+import imutils
+import argparse
+
+
+def main():
+    """Main function for multi object tracking
+    Args:
+        -v name of video file
+        -b name of background model to be used for object detection
+        The background models to be made available include mean, median,
+        Gaussian, mixture of Gaussians, and K-Nearest Neighbor
+        The specific string to be used to specify the background modeling approach are
+        'mean' for mean
+        'median for median
+        'gaussian' for Gaussian
+        'mog' for Mixture of Gaussians, and
+        'knn' for K-Nearest Neighbor
+    Return:
+        None
+    Example usage:
+        python cv_hw4.py -v QIL_orig.mp4 -b knn -d 0
+    """
+
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video", type=str,
+                    help="name of input video file")
+    ap.add_argument("-b", "--bgmodel", required=True,
+                    help="type of background modeling (mean, median, gaussian, mog, or knn")
+    ap.add_argument("-d", "--debug", type=int, default=0,
+                    help="debug flag (set to 1 to display images)")
+
+    args = vars(ap.parse_args())
+
+    # Debug value to decide if images are displayed or not
+    debug = args["debug"]
+
+    # Create opencv video capture object
+    cap = cv2.VideoCapture(args["video"])
+
+    # Create Object Detector
+    detector = Detectors(args["bgmodel"],debug)
+
+    # Create Object Tracker
+    # dist_thresh: 15 - Search within this distance to find match
+    # max_frames_to_skip: 30 - delete objects with cannot match for these number of frames
+    # max_trace_length: 1000 - maximum length of a trajectory
+    # trackIdCount: 1 - starting count for objects
+    tracker = Tracker(15, 30, 1000, 1)
+
+    # Variables initialization
+    skip_frame_count = 0
+    track_colors = np.random.uniform(0, 255, size=(10000, 3))
+    frame_count = 0
+    min_track_length = 50
+
+    pause = False
+
+    # Infinite loop to process video frames
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret:
+            frame_count += 1
+
+            # Make copy of original frame
+            orig_frame = copy.copy(frame)
+            last_frame = copy.copy(frame)
+
+            # Detect and return centroids of the objects in the frame
+            centers = detector.DetectBlob(frame)
+
+            # If centroids are detected then track them
+            if (len(centers) > 0):
+
+                # Track object using Kalman Filter
+                tracker.Update(centers)
+
+                # For identified object tracks draw tracking line if object is tracked for
+                # longer then min_track_length frames
+                # Use various colors to indicate different track_id
+                for i in range(len(tracker.tracks)):
+                    if (len(tracker.tracks[i].trace) > min_track_length):
+                        for j in range(len(tracker.tracks[i].trace)-1):
+                            # Draw trace line
+                            x1 = tracker.tracks[i].trace[j][0][0]
+                            y1 = tracker.tracks[i].trace[j][1][0]
+                            x2 = tracker.tracks[i].trace[j+1][0][0]
+                            y2 = tracker.tracks[i].trace[j+1][1][0]
+                            clr = tracker.tracks[i].track_id % 9999
+                            cv2.line(frame, (int(x1), int(y1)), (int(x2), int(y2)),
+                                     track_colors[clr], 2)
+
+                # Display the resulting tracking frame
+                if debug == 1:
+                    cv2.imshow('Tracking', frame)
+
+            # Display the original frame
+            if debug == 1:
+                cv2.imshow('Original', orig_frame)
+
+            # Slower the FPS
+            cv2.waitKey(1)
+
+            # Check for key strokes
+            k = cv2.waitKey(1) & 0xff
+
+            # if the `q` key was pressed, break from the loop
+            if k == ord("q"):
+                break
+            if k == 112:  # 'p' has been pressed. this will pause/resume the code.
+                pause = not pause
+                if (pause is True):
+                    print("Code is paused. Press 'p' to resume..")
+                    print("Object ID: {}: {}".format(tracker.tracks[i].track_id, len(tracker.tracks[i].trace)))
+                    while (pause is True):
+                        # stay in this loop until
+                        key = cv2.waitKey(30) & 0xff
+                        if key == 112:
+                            pause = False
+                            print("Resume code..!!")
+                            break
+        else:
+            # For identified object tracks draw tracking line if object is tracked for
+            # longer then min_track_length frames
+            # Use various colors to indicate different track_id
+            tracked_object_count = 0
+            for i in range(len(tracker.tracks)):
+                if (len(tracker.tracks[i].trace) > min_track_length):
+                    tracked_object_count += 1
+                    for j in range(len(tracker.tracks[i].trace) - 1):
+                        # Draw trace line
+                        x1 = tracker.tracks[i].trace[j][0][0]
+                        y1 = tracker.tracks[i].trace[j][1][0]
+                        x2 = tracker.tracks[i].trace[j + 1][0][0]
+                        y2 = tracker.tracks[i].trace[j + 1][1][0]
+                        clr = tracker.tracks[i].track_id % 9999
+                        cv2.line(last_frame, (int(x1), int(y1)), (int(x2), int(y2)),
+                                 track_colors[clr], 2)
+
+            text = "Total Objects Detected and Tracked: {}".format(tracked_object_count)
+            cv2.putText(last_frame, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            text = "Total Video Frames: {}".format(frame_count)
+            cv2.putText(last_frame, text, (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            text = "Background Model: {}".format(args["bgmodel"])
+            cv2.putText(last_frame, text, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+            # Write the result image
+            result_file = "TrackingResult-"+args["bgmodel"]+".png"
+            cv2.imwrite(result_file, last_frame)
+
+            # Display the resulting tracking frame
+            if debug == 1:
+                cv2.imshow('All Tracked Objects', last_frame)
+                cv2.waitKey(0)
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    # execute main
+    main()
